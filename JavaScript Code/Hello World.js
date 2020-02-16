aff9168952a3fa0e2d5983dffc9fc35289366972
CaidenTousaw <57578640+CaidenTousaw@users.noreply.github.com>
2020-02-15 21:33:41 -0800

    Will tell you if you have a bad name

---
+++ JavaScript Code/Hello World.js
@@ -0,0 +1,26 @@
+//This is a Hello World Command
+console.log("Hello World")
+
+//Simple User Input, Will pop-up window that asks for your name
+var name = window.prompt("Enter your name: ");
+alert("Your name is " + name);
+
+//Adding some simple Logic
+var name = window.prompt("Enter your name: ");
+if (name == "Amy") {
+  alert("You have a bad name")
+} else if (name == "Adam") {
+  alert("You have a bad name")
+} else if (name == "Bob") {
+  alert("You have a bad name")
+} else if (name == "Ethan") {
+  alert("You have a bad name")
+} else if (name == "Evan") {
+  alert("You have a bad name")
+} else if (name == "Nick") {
+  alert("You have a bad name")
+} else if (name == "Kevin") {
+  alert("You have a bad name")
+} else {
+  alert("Your name is " + name)
+}
